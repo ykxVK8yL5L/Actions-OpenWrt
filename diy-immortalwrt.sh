bc2ad2c97787953beac895982e54357a6e795c68
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-11-25 00:26:52 +0800

    Update diy-immortalwrt.sh

--- diy-immortalwrt.sh
+++ diy-immortalwrt.sh
@@ -43,8 +43,8 @@ cp -rf luci-app-dockerman/applications/luci-app-dockerman package/luci-app-docke
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/luci-app-autoipsetadder
 git clone --depth=1 https://github.com/mchome/openwrt-dogcom.git package/openwrt-dogcom
 git clone --depth=1 https://github.com/mchome/luci-app-dogcom.git package/luci-app-dogcom
-git clone --depth=1 https://github.com/vernesong/OpenClash.git
-cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
+#git clone --depth=1 https://github.com/vernesong/OpenClash.git
+#cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
 pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
