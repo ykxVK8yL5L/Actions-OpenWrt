938196faa91317a375b8ef3ce8a22430596572fb
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2024-11-25 00:33:16 +0800

    Update diy-immortalwrt.sh

--- diy-immortalwrt.sh
+++ diy-immortalwrt.sh
@@ -46,9 +46,9 @@ git clone --depth=1 https://github.com/mchome/luci-app-dogcom.git package/luci-a
 #git clone --depth=1 https://github.com/vernesong/OpenClash.git
 #cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+#pushd package/luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd
 #rm -rf OpenClash
 
 
